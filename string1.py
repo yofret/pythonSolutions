2c6929137f66be6c3c05d7da2def3396de34742f
Yofret <riosmer28@gmail.com>
2017-03-02 20:46:02 -0500

    Adding solutions to Strings1 round excersices

--- string1.py
+++ string1.py
@@ -24,8 +24,10 @@
 # So donuts(5) returns 'Number of donuts: 5'
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
-  # +++your code here+++
-  return
+  if count >= 10:
+    return 'Number of donuts: many'
+  else:
+    return 'Number of donuts: %d'  % (count)
 
 
 # B. both_ends
@@ -34,8 +36,10 @@ def donuts(count):
 # so 'spring' yields 'spng'. However, if the string length
 # is less than 2, return instead the empty string.
 def both_ends(s):
-  # +++your code here+++
-  return
+  if len(s) < 2:
+    return ''
+  else:
+    return s[0:2] + s[-2:]
 
 
 # C. fix_start
@@ -48,8 +52,8 @@ def both_ends(s):
 # Hint: s.replace(stra, strb) returns a version of string s
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
-  # +++your code here+++
-  return
+  firstLetter = s[0]
+  return firstLetter + s[1:].replace(firstLetter,'*')
 
 
 # D. MixUp
@@ -60,8 +64,9 @@ def fix_start(s):
 #   'dog', 'dinner' -> 'dig donner'
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
-  # +++your code here+++
-  return
+  firstLettersA = a[0:2]
+  firstLettersB = b[0:2]
+  return firstLettersB + a[2:] + ' ' + firstLettersA + b[2:]
 
 
 # Provided simple test() function used in main() to print
